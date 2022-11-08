7c81fd24dfacb126c3f4eba7814a8d322e459394
Indierock00 <52739181+Indierock00@users.noreply.github.com>
2022-11-08 15:24:43 +0100

    Voltage

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_markw.mk
+    $(LOCAL_DIR)/voltage_markw.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_markw-user \
-    lineage_markw-userdebug \
-    lineage_markw-eng
+    voltage_markw-user \
+    voltage_markw-userdebug \
+    voltage_markw-eng
