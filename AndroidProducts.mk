3c8ff754646a01ee53751e8b45b7351aca3a3aea
Indierock00 <52739181+Indierock00@users.noreply.github.com>
2022-11-18 14:16:47 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_markw.mk
+    $(LOCAL_DIR)/superior_markw.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_markw-user \
-    lineage_markw-userdebug \
-    lineage_markw-eng
+    superior_markw-user \
+    superior_markw-userdebug \
+    superior_markw-eng
