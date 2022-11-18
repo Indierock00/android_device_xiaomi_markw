03a0381924efbd744ff766e4cf0f52605d47ff4c
Indierock00 <52739181+Indierock00@users.noreply.github.com>
2022-11-18 14:22:52 +0100

    Update and rename lineage_markw.mk to superior_markw.mk

--- superior_markw.mk
+++ superior_markw.mk
@@ -21,12 +21,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from markw device
 $(call inherit-product, device/xiaomi/markw/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common SuperiorOS stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
+
+#SuperiorOS Flags
+SUPERIOR_GAPPS := minimal
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := markw
-PRODUCT_NAME := lineage_markw
+PRODUCT_NAME := superior_markw
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 4 Prime
 PRODUCT_MANUFACTURER := Xiaomi
